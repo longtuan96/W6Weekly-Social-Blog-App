2a7464f883d915d3de50208777e26927f90de0d7
Hue203 <huenguyenmt.vn@gmail.com>
2021-04-23 15:21:18 +0700

    test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>this is HomePage</h1>
+    </div>
+  );
 };
 
 export default HomePage;
